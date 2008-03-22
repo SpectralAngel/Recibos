7e1de9be47824c31329a14352a783de5121aee2f
Carlos Flores <cafg10@gmail.com>
2008-03-22 14:50:19 -0600

    Añadida plantilla de impresión.

--- recibos/release.py
+++ recibos/release.py
@@ -4,11 +4,11 @@ version = "1.0"
 
 # description = "Your plan to rule the world"
 # long_description = "More description about your plan"
-# author = "Your Name Here"
-# email = "YourEmail@YourDomain"
-# copyright = "Vintage 2006 - a good year indeed"
+author = "Carlos Flores"
+email = "cafg10@gmail.com"
+copyright = "Carlos Flores 2008"
 
 # if it's open source, you might want to specify these
 # url = "http://yourcool.site/"
 # download_url = "http://yourcool.site/download"
-# license = "MIT"
+license = "GPL"
