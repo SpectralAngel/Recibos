5c5e8a5ab6bf2e29f15c05a9744f110a5d57b7cb
Carlos Flores <cafg10@gmail.com>
2025-02-25 10:54:17 -0600

    ✨ Update cookie name and table names for Ceiba isolation

--- recibos/model.py
+++ recibos/model.py
@@ -205,7 +205,7 @@ class Visit(Entity):
     """
     A visit to your site
     """
-    using_options(tablename='visit')
+    using_options(tablename='visit_ceiba')
 
     visit_key = Field(String(40), primary_key=True)
     created = Field(DateTime, nullable=False, default=datetime.now)
@@ -220,7 +220,7 @@ class VisitIdentity(Entity):
     """
     A Visit that is link to a User object
     """
-    using_options(tablename='visit_identity')
+    using_options(tablename='visit_identity_ceiba')
 
     visit_key = Field(String(40), primary_key=True)
     user = ManyToOne('User', colname='user_id', use_alter=True)
